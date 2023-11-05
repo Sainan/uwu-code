81e9700a6141408d21bf07d866264a8643153818
Sainan <sainan@calamity.gg>
2023-11-05 22:57:59 +0100

    Simplify sequence to key mapping

--- main.cpp
+++ main.cpp
@@ -17,84 +17,81 @@ using namespace soup;
 	return str;
 }
 
-int main(int argc, const char** argv)
+[[nodiscard]] static Key sequenceToKey(uint16_t sequence)
 {
-	Key key_map[0b111'111'111 + 1];
-	for (auto& key : key_map)
+	switch (sequence)
 	{
-		key = KEY_NONE;
-	}
-	key_map[0b100'100] = KEY_A;         // #__ #__
-	key_map[0b100'010] = KEY_B;         // #__ _#_
-	key_map[0b100'001] = KEY_C;         // #__ __#
-	key_map[0b100'110] = KEY_D;         // #__ ##_
-	key_map[0b100'011] = KEY_E;         // #__ _##
-	key_map[0b100'101] = KEY_F;         // #__ #_#
-	key_map[0b100'111] = KEY_G;         // #__ ###
-	key_map[0b010'100] = KEY_H;         // _#_ #__
-	key_map[0b010'010] = KEY_I;         // _#_ _#_
-	key_map[0b010'001] = KEY_J;         // _#_ __#
-	key_map[0b010'110] = KEY_K;         // _#_ ##_
-	key_map[0b010'011] = KEY_L;         // _#_ _##
-	key_map[0b010'101] = KEY_M;         // _#_ #_#
-	key_map[0b010'111] = KEY_N;         // _#_ ###
-	key_map[0b001'100] = KEY_O;         // __# #__
-	key_map[0b001'010] = KEY_P;         // __# _#_
-	key_map[0b001'001] = KEY_Q;         // __# __#
-	key_map[0b001'110] = KEY_R;         // __# ##_
-	key_map[0b001'011] = KEY_S;         // __# _##
-	key_map[0b001'101] = KEY_T;         // __# #_#
-	key_map[0b001'111] = KEY_U;         // __# ###
-	key_map[0b101'100] = KEY_V;         // #_# #__
-	key_map[0b101'010] = KEY_W;         // #_# _#_
-	key_map[0b101'001] = KEY_X;         // #_# __#
-	key_map[0b101'110] = KEY_Y;         // #_# ##_
-	key_map[0b101'011] = KEY_Z;         // #_# _##
-	key_map[0b101'101] = KEY_SPACE;     // #_# #_#
-	key_map[0b101'111] = KEY_PERIOD;    // #_# ###
-	key_map[0b110'100] = KEY_1;         // ##_ #__
-	key_map[0b110'010] = KEY_2;         // ##_ _#_
-	key_map[0b110'001] = KEY_3;         // ##_ __#
-	key_map[0b110'110] = KEY_4;         // ##_ ##_
-	key_map[0b110'011] = KEY_5;         // ##_ _##
-	key_map[0b110'101] = KEY_6;         // ##_ #_#
-	key_map[0b110'111] = KEY_7;         // ##_ ###
-	key_map[0b011'100] = KEY_8;         // _## #__
-	key_map[0b011'010] = KEY_9;         // _## _#_
-	key_map[0b011'001] = KEY_0;         // _## __#
-	key_map[0b011'110] = KEY_BACKSPACE; // _## ##_
-	key_map[0b011'011] = KEY_ENTER;     // _## _##
-	key_map[0b011'101'100] = KEY_BRACKET_LEFT; // _## #_# #__
-	key_map[0b011'101'010] = KEY_BACKSLASH;    // _## #_# _#_
-	key_map[0b011'101'001] = KEY_BRACKET_RIGHT; // _## #_# __#
-	key_map[0b011'101'110] = KEY_SEMICOLON;    // _## #_# ##_
-	key_map[0b011'101'011] = KEY_SLASH;        // _## #_# _##
-	key_map[0b011'101'101] = KEY_QUOTE;        // _## #_# #_#
-	key_map[0b011'101'111] = KEY_MINUS;        // _## #_# ###
-	key_map[0b011'111'100] = KEY_EQUALS;       // _## ### #__
-	key_map[0b011'111'010] = KEY_COMMA;        // _## ### _#_
+	case 0b100'100: return KEY_A;         // #__ #__
+	case 0b100'010: return KEY_B;         // #__ _#_
+	case 0b100'001: return KEY_C;         // #__ __#
+	case 0b100'110: return KEY_D;         // #__ ##_
+	case 0b100'011: return KEY_E;         // #__ _##
+	case 0b100'101: return KEY_F;         // #__ #_#
+	case 0b100'111: return KEY_G;         // #__ ###
+	case 0b010'100: return KEY_H;         // _#_ #__
+	case 0b010'010: return KEY_I;         // _#_ _#_
+	case 0b010'001: return KEY_J;         // _#_ __#
+	case 0b010'110: return KEY_K;         // _#_ ##_
+	case 0b010'011: return KEY_L;         // _#_ _##
+	case 0b010'101: return KEY_M;         // _#_ #_#
+	case 0b010'111: return KEY_N;         // _#_ ###
+	case 0b001'100: return KEY_O;         // __# #__
+	case 0b001'010: return KEY_P;         // __# _#_
+	case 0b001'001: return KEY_Q;         // __# __#
+	case 0b001'110: return KEY_R;         // __# ##_
+	case 0b001'011: return KEY_S;         // __# _##
+	case 0b001'101: return KEY_T;         // __# #_#
+	case 0b001'111: return KEY_U;         // __# ###
+	case 0b101'100: return KEY_V;         // #_# #__
+	case 0b101'010: return KEY_W;         // #_# _#_
+	case 0b101'001: return KEY_X;         // #_# __#
+	case 0b101'110: return KEY_Y;         // #_# ##_
+	case 0b101'011: return KEY_Z;         // #_# _##
+	case 0b101'101: return KEY_SPACE;     // #_# #_#
+	case 0b101'111: return KEY_PERIOD;    // #_# ###
+	case 0b110'100: return KEY_1;         // ##_ #__
+	case 0b110'010: return KEY_2;         // ##_ _#_
+	case 0b110'001: return KEY_3;         // ##_ __#
+	case 0b110'110: return KEY_4;         // ##_ ##_
+	case 0b110'011: return KEY_5;         // ##_ _##
+	case 0b110'101: return KEY_6;         // ##_ #_#
+	case 0b110'111: return KEY_7;         // ##_ ###
+	case 0b011'100: return KEY_8;         // _## #__
+	case 0b011'010: return KEY_9;         // _## _#_
+	case 0b011'001: return KEY_0;         // _## __#
+	case 0b011'110: return KEY_BACKSPACE; // _## ##_
+	case 0b011'011: return KEY_ENTER;     // _## _##
+	case 0b011'101'100: return KEY_BRACKET_LEFT;  // _## #_# #__
+	case 0b011'101'010: return KEY_BACKSLASH;     // _## #_# _#_
+	case 0b011'101'001: return KEY_BRACKET_RIGHT; // _## #_# __#
+	case 0b011'101'110: return KEY_SEMICOLON;     // _## #_# ##_
+	case 0b011'101'011: return KEY_SLASH;         // _## #_# _##
+	case 0b011'101'101: return KEY_QUOTE;         // _## #_# #_#
+	case 0b011'101'111: return KEY_MINUS;         // _## #_# ###
+	case 0b011'111'100: return KEY_EQUALS;        // _## ### #__
+	case 0b011'111'010: return KEY_COMMA;         // _## ### _#_
 
 	// Dead end/unused sequences
-	key_map[0b011'111'001] = KEY_OEM_1; // _## ### __#
-	key_map[0b011'111'110] = KEY_OEM_1; // _## ### ##_
-	key_map[0b011'111'011] = KEY_OEM_1; // _## ### _##
-	key_map[0b011'111'101] = KEY_OEM_1; // _## ### #_#
-	key_map[0b011'111'111] = KEY_OEM_1; // _## ### ###
+	case 0b011'111'001: return KEY_OEM_1; // _## ### __#
+	case 0b011'111'110: return KEY_OEM_1; // _## ### ##_
+	case 0b011'111'011: return KEY_OEM_1; // _## ### _##
+	case 0b011'111'101: return KEY_OEM_1; // _## ### #_#
+	case 0b011'111'111: return KEY_OEM_1; // _## ### ###
 
 	// Sequences that can only be reached with shift
-	key_map[0b111'100] = KEY_LCTRL; // ### ### #__
-	key_map[0b111'010] = KEY_OEM_1; // ### ### _#_
-	key_map[0b111'001] = KEY_OEM_1; // ### ### __#
-	key_map[0b111'110] = KEY_OEM_1; // ### ### ##_
-	key_map[0b111'011] = KEY_OEM_1; // ### ### _##
-	key_map[0b111'101] = KEY_OEM_1; // ### ### #_#
-	key_map[0b111'111] = KEY_CAPS_LOCK; // ### ### ###
+	case 0b111'100: return KEY_LCTRL; // ### ### #__
+	case 0b111'010: return KEY_OEM_1; // ### ### _#_
+	case 0b111'001: return KEY_OEM_1; // ### ### __#
+	case 0b111'110: return KEY_OEM_1; // ### ### ##_
+	case 0b111'011: return KEY_OEM_1; // ### ### _##
+	case 0b111'101: return KEY_OEM_1; // ### ### #_#
+	case 0b111'111: return KEY_CAPS_LOCK; // ### ### ###
+	}
+	return KEY_NONE;
+}
 
-	// Also consider that the following sequences could be given special meanings:
-	// - Shift + Space     (### #_# #_#)
-	// - Shift + Backspace (### _## ##_)
-	// - Shift + Enter     (### _## _##)
-
+int main(int argc, const char** argv)
+{
 	DigitalKeyboard kbd;
 	bool shift = false;
 	bool ctrl = false;
@@ -131,12 +128,7 @@ int main(int argc, const char** argv)
 				}
 				current = 0;
 
-				SOUP_IF_UNLIKELY (sequence >= COUNT(key_map))
-				{
-					std::cout << "[ERROR: Dead end that was not mapped]";
-					goto _reset_sequence_data;
-				}
-				if (auto key = key_map[sequence]; key != KEY_NONE) // End of sequence?
+				if (auto key = sequenceToKey(sequence); key != KEY_NONE) // End of sequence?
 				{
 					if (key == KEY_LCTRL)
 					{
